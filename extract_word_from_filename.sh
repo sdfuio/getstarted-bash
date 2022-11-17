2429e518c7ae39beccc00047c99dbc7c6f512f3f
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-11-17 10:02:19 -0500

    Update extract_word_from_filename.sh

--- extract_word_from_filename.sh
+++ extract_word_from_filename.sh
@@ -7,10 +7,10 @@ echo "full filename is ${full_fname}"
 
 ext=$(echo $full_fname | egrep [^\.]+$ -o) # jpg
 echo "${ext} is the extension of ${full_fname}" 
-echo "this is equivalent to ${full_fname%%.${ext}}"
 
 fname=$(echo $full_fname | egrep ^[a-z]+[0-9]+ -o) # abcdefg4321
 echo "extension ${ext} is removed -> ${fname} "
+echo "this is equivalent to ${full_fname%%.${ext}}"
 
 num=$(echo $fname | egrep [0-9]+$ -o) # 4321
 echo "number is ${num}"
